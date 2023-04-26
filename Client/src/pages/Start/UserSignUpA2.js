cc31a0d4eeb00e74762c94a5bbb0b13b0e0fe4b9
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-04-25 21:57:16 -0400

    Update form UserSignUp Page

--- Client/src/pages/Start/UserSignUpA2.js
+++ Client/src/pages/Start/UserSignUpA2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserSignUpA2.css";
 import HumanCityLogo from '../../assets/HumanCityLogo.png';
 import sslogo from '../../assets/sslogo.png';
@@ -11,6 +11,16 @@ import projectsignin from '../../assets/projectsignin.png';
 import { Link } from "react-router-dom";
 
 const UserSignUp = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [acceptedTerms, setAcceptedTerms] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // handle form submission here
+    }
 
     return (
         <>
@@ -26,20 +36,51 @@ const UserSignUp = () => {
             <div className="signuptitle">Let's get you stitched in!</div>
             <div className="signupdescription">We're designing and creating an open ecosystem; so innovative ideas can turn into world changing action. </div>
             
-            <input className="email" type="text" placeholder="    Email" id="email" name="email"></input>
-            <br/>
-            <input className="password" type="password" placeholder="    Password"id="password" name="password"></input>
-            <br/>
-            <input className="password" type="password" placeholder="    Confirm Password" id="confirmpassword" name="confirmpassword"></input>
-            <br/>
-            <div className="checkarea">
-                <input className="checkboxpolicy" type="checkbox" />
-                <div className="policy">I accept Spotstitch's <span className="bold">Privacy Policy</span> and <span className="bold">Terms of Service</span>.</div>
-            </div>
-            <br/>
+            <form className="formsignin" onSubmit={handleSubmit}>
+                <input 
+                    className="email" 
+                    type="text" 
+                    placeholder="     Email" 
+                    id="email" 
+                    name="email" 
+                    value={email} 
+                    onChange={(event) => setEmail(event.target.value)} 
+                />
+                <br/>
+                <input 
+                    className="password" 
+                    type="password" 
+                    placeholder="     Password"
+                    id="password" 
+                    name="password" 
+                    value={password} 
+                    onChange={(event) => setPassword(event.target.value)} 
+                />
+                <br/>
+                <input 
+                    className="password" 
+                    type="password" 
+                    placeholder="     Confirm Password" 
+                    id="confirmpassword" 
+                    name="confirmpassword" 
+                    value={confirmPassword} 
+                    onChange={(event) => setConfirmPassword(event.target.value)} 
+                />
+                <br/>
+                <div className="checkarea">
+                    <input 
+                        className="checkboxpolicy" 
+                        type="checkbox" 
+                        checked={acceptedTerms} 
+                        onChange={(event) => setAcceptedTerms(event.target.checked)} 
+                    />
+                    <div className="policy">I accept Spotstitch's <span className="bold">Privacy Policy</span> and <span className="bold">Terms of Service</span>.</div>
+                </div>
+                <br/>
+                <Link to='/accountsetup'className="lacc"> <input className="signup" type="submit" value="Join Spotstitch"></input></Link>  
                 
-            <Link to='/accountsetup'className="lacc"> <input className="signup" type="submit" value="Join Spotstitch"></input></Link>  
-            <br/>
+            </form>
+             <br/>
             <img className="lineor" src={orline}  alt="orline"/>
             <br/>
             <button className="googlebutton">
