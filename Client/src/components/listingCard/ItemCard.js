617a17d9d39109d69b3fa25215244fd8efadeff8
Jacky Shen <83739233+ElusoryPaths@users.noreply.github.com>
2023-05-24 19:07:35 -0400

    updated item cards for inventory

--- Client/src/components/listingCard/ItemCard.js
+++ Client/src/components/listingCard/ItemCard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 import './listingCard.css';
+import { Col } from 'react-bootstrap';
 
 const ItemCard = ({ item = {} }) => {
     const [tags, setTags] = useState([]);
@@ -11,29 +12,31 @@ const ItemCard = ({ item = {} }) => {
         if (item.tags) {
             setTags(
                 item.tags.map((tag, index) => (
-                    <EventTag key={tag} eventTag={tag} />
+                    <EventTag key={`item_${index}`} tag={tag} />
                 ))
             );
         }
     }, []);
     return (
         <div
-            className='card my-3 mx-2 shadow listingCard'
-            style={{ borderRadius: '25px' }}
+            className='card my-3 mx-2 listingCard'
+            style={{ borderRadius: '20px' }}
         >
             <img
                 src={item.img ? item.img : require('../../assets/holderimg.png')}
                 className='card-img-top '
-                style={{ borderRadius: '25px 25px 0px 0px', height: '13vh' }}
+                height={'180px'}
+                width={'250px'}
+                style={{ borderRadius: '20px 20px 0px 0px', height: '13vh' }}
             />
             <div className='card-body' style={{ fontSize: 'smaller' }}>
-                <h5 className='card-title fs-6'>{item.title}</h5>
-                <p className='card-text mb-1'>
+                <h5 className='card-title fs-20 m-0'>{item.title}</h5>
+                <p className='card-text mb-1 fs-14'>
                     {item.description}
                     <br />
                     {item.rating}
                 </p>
-                <div className='row row-cols-2 g-0'>
+                <div className='row row-cols-2 g-0 mt-1 '>
                     {tags.length > 0 ? tags : null}
                 </div>
             </div>
@@ -41,20 +44,16 @@ const ItemCard = ({ item = {} }) => {
     );
 };
 
-const EventTag = ({ eventTag }) => {
+const EventTag = ({ tag }) => {
     return (
-        <div className='col text-nowrap text-center mt-1'>
-            <div
-                className='rounded-pill px-2'
-                style={{ backgroundColor: '#D9D9D9', fontSize: '90%' }}
-            >
-                {eventTag}{' '}
-                <AiOutlineClose
-                    className='ms-2 me-1'
-                    style={{ backgroundColor: 'white', borderRadius: '25px' }}
-                />
-            </div>
-        </div>
+        <Col className="light rounded-pill me-2 py-1 fs-10" lg={5}>
+            <span>
+                <div className='mx-2 d-inline'>{tag}</div>
+                <div className='float-end mx-2'>
+                    <AiOutlineClose style={{ backgroundColor: 'white', borderRadius: '25px' }} />
+                </div>
+            </span>
+        </Col>
     );
 };
 
--- Client/src/pages/Inventory/Inventory.js
+++ Client/src/pages/Inventory/Inventory.js
@@ -1,21 +1,35 @@
 import { Col, Container, Row, Card } from "react-bootstrap";
 import ItemCard from "../../components/listingCard/ItemCard";
+import { useEffect, useState } from "react";
 
 function Inventory() {
+  const [items, setItems] = useState([])
+
+  const itemEx = [
+    { title: 'item name', description: 'Description of the item', rating: 'Trader rating', tags: ['item tag', 'item tag'] },
+    { title: 'item name', description: 'Description of the item', rating: 'Trader rating', tags: ['item tag', 'item tag'] },
+    { title: 'item name', description: 'Description of the item', rating: 'Trader rating', tags: ['item tag', 'item tag'] },
+    { title: 'item name', description: 'Description of the item', rating: 'Trader rating', tags: ['item tag', 'item tag'] },
+    { title: 'item name', description: 'Description of the item', rating: 'Trader rating', tags: ['item tag', 'item tag'] }
+  ]
+
   return <Container className="my-4">
     <Row className="mt-3 content-border-l round-s">
       <Col>
-            <div className="d-flex justify-content-evenly">
-              <button className={"btn-nav active p-3"}>
-                <p className="nopadding s-16 fw-500">Inventory</p>
-              </button>
-            </div>
+        <div className="d-flex justify-content-evenly">
+          <button className={"btn-nav active p-3"}>
+            <p className="nopadding s-16 fw-500">Inventory</p>
+          </button>
+        </div>
       </Col>
     </Row>
     <Row className="mt-3 content-border-l round-s">
-      <Col lg={3}>
-        <ItemCard item={{name: 'test', description: 'test', rating:0 }}/>
-      </Col>
+      {itemEx.map(item=>{
+        return <Col lg={3} className="">
+          <ItemCard item={ item } />
+        </Col>
+      })}
+      
     </Row>
   </Container>
 }
