02b554a91e506c311b2af45dcd362ca342f65560
ElusoryPaths <jacky.meepatnight@gmail.com>
2023-05-25 17:50:38 -0400

    setup profile

--- Client/src/pages/Profile/Profile.js
+++ Client/src/pages/Profile/Profile.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./Profile.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Profile = () => {
 
     return (
-        <div className="container">
-            <div className="title">
-                Profile
-            </div>
+        <div>
+            <Container className="my-4">
+                <Row className="mt-3 content-border-l round-s">
+                    <Col>
+                        <div className="d-flex justify-content-evenly">
+                            <button className={"btn-nav active p-3"}>
+                                <p className="nopadding s-16 fw-500">Profile</p>
+                            </button>
+                        </div>
+                    </Col>
+                </Row>
+                
+            </Container>
         </div>
+
     );
 };
 
