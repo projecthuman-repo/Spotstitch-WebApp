c3b100fb527cb51d4756ad9e877f492893759e1f
Jacky Shen <83739233+ElusoryPaths@users.noreply.github.com>
2023-06-02 12:41:36 -0400

    add new card done

--- Client/src/pages/Wallet/AddCard.js
+++ Client/src/pages/Wallet/AddCard.js
@@ -35,6 +35,7 @@ function AddCard() {
                 aria-labelledby="modal"
                 backdrop="static"
                 size="lg"
+                centered
             >
                 <Modal.Header className="underline" closeButton>
                     <Row className="w-100">
@@ -47,28 +48,38 @@ function AddCard() {
                 <Modal.Body >
                     <Form>
                         <Row>
-                            <Col>
+                            <Col lg={1}></Col>
+                            <Col lg={6}>
 
                                 <Form.Group>
                                     <Form.Label>Card number</Form.Label>
                                     <Form.Control type="number"></Form.Control>
                                 </Form.Group>
-                                <Form.Group>
+                                <Form.Group className="mt-2">
                                     <Form.Label>Expiration date</Form.Label>
                                     <Row>
-                                        <Col><Form.Control type='number' value={date.getMonth()+1}></Form.Control></Col>
-                                        <Col><Form.Control type="number" value={date.getFullYear()}></Form.Control></Col>
+                                        <Col lg={5}><Form.Control type='number' value={date.getMonth() + 1}></Form.Control></Col>
+                                        <Col lg={5}><Form.Control type="number" value={date.getFullYear()}></Form.Control></Col>
                                     </Row>
-
-
                                 </Form.Group>
                             </Col>
-                            <Col>
+                            <Col lg={4}>
                                 <Form.Group>
                                     <Form.Label>Name on card</Form.Label>
                                     <Form.Control type="text"></Form.Control>
                                 </Form.Group>
+                                <Row>
+                                    <Col lg={7}><Form.Group className="mt-2">
+                                        <Form.Label>CVV</Form.Label>
+                                        <Form.Control type="text"></Form.Control>
+                                    </Form.Group></Col>
+                                </Row>
+
                             </Col>
+                            <Col lg={1}></Col>
+                        </Row>
+                        <Row className="mt-3">
+                            <Col><button className="btn p-2 px-5 mx-3 btn-card float-end">Save</button></Col>
                         </Row>
                     </Form>
 
