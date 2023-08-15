03f816ed68d28c6d2fdf2410c998d083342f44bc
Jacky Shen <83739233+ElusoryPaths@users.noreply.github.com>
2023-08-15 17:55:33 -0400

    update post and product routes

--- Server/src/routes/api/products/index.js
+++ Server/src/routes/api/products/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const allProducts = require('./allProducts');
+const getProduct = require('./getProduct');
+const createProduct = require('./createProduct');
 
 // Create a router on which to mount our API endpoints
 const router = express.Router();
@@ -8,11 +10,13 @@ const router = express.Router();
  * Define routes for products below
  */
 
+router.get('/:id', getProduct)
+
 // get all products
 router.get('/all', allProducts)
 
 // create new product
-router.post('/create')
+router.post('/create', createProduct)
 
 // update existing product
 router.put('/update')
