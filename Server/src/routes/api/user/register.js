a649b0887deb55676254596d0af6df4fde3e90ec
Ali Amin <aliamin745@gmail.com>
2024-03-01 13:38:28 -0500

    Updated query

--- Server/src/routes/api/user/register.js
+++ Server/src/routes/api/user/register.js
@@ -1,44 +1,47 @@
-const { User } = require('../../../model');
-const { CrossPlatformUser } = require('../../../model');
+const { User } = require("../../../model");
+const { CrossPlatformUser } = require("../../../model");
 
 module.exports = async (req, res) => {
-  try {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      phoneNumber: req.body.phoneNumber,
-      password: req.body.password,
-      picture: req.body.picture,
-    });
-    await User.create(user);
+	try {
+		const user = new User({
+			username: req.body.username,
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			userType: req.body.userType,
+			email: req.body.email,
+			phoneNumber: req.body.phoneNumber,
+			password: req.body.password,
+			picture: req.body.picture,
+		});
+		await User.create(user);
 
-    // CrossPlatformUser manages the 3 web app users (lotuslearning, regenquest, spotstitch)
-    // If the CrossPlatformUser has not been created with other platform, create a new one
-    const crossPlatformUserExists = await CrossPlatformUser.findOne({
-      $or: [{ email: user.email }, { phoneNumber: user.phoneNumber }],
-    });
+		// CrossPlatformUser manages the 3 web app users (lotuslearning, regenquest, spotstitch)
+		// If the CrossPlatformUser has not been created with other platform, create a new one
+		const crossPlatformUserExists = await CrossPlatformUser.findOne({
+			$or: [{ email: user.email }, { phoneNumber: user.phoneNumber }],
+		});
 
-    if (crossPlatformUserExists) {
-      crossPlatformUserExists.spotstitchUserId = user._id;
-      await crossPlatformUserExists.save();
-    } else {
-      const newCrossPlatformUser = new CrossPlatformUser({
-        email: user.email,
-        phoneNumber: user.phoneNumber,
-        spotstitchUserId: user._id,
-      });
-      await newCrossPlatformUser.save();
-    }
+		if (crossPlatformUserExists) {
+			crossPlatformUserExists.spotstitchUserId = user._id;
+			await crossPlatformUserExists.save();
+		} else {
+			const newCrossPlatformUser = new CrossPlatformUser({
+				email: user.email,
+				phoneNumber: user.phoneNumber,
+				spotstitchUserId: user._id,
+			});
+			await newCrossPlatformUser.save();
+		}
 
-    res.status(201).json(user);
-  } catch (e) {
-    let msg;
-    if (e.code == 11000) {
-      msg = 'User already exists';
-    } else {
-      msg = e.message;
-    }
-    console.log(e);
-    res.status(400).json(msg);
-  }
+		res.status(201).json(user);
+	} catch (e) {
+		let msg;
+		if (e.code == 11000) {
+			msg = "User already exists";
+		} else {
+			msg = e.message;
+		}
+		console.log(e);
+		res.status(400).json(msg);
+	}
 };
