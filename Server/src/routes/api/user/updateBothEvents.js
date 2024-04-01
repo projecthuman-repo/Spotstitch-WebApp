08ddcae2bb5616dcd8da6b5a1fc8acbe04a64cb4
m-shahzad1 <114366406+m-shahzad1@users.noreply.github.com>
2024-04-01 02:35:00 -0400

    update events for crossplatform

--- Server/src/routes/api/user/updateBothEvents.js
+++ Server/src/routes/api/user/updateBothEvents.js
@@ -1,111 +1,57 @@
-const {CrossPlatformEvent} = require('../../../model');
-const {Event} = require('../../../model');
+const { Event, CrossPlatformEvent } = require('../../../model');
 const logger = require('../../../logger');
 const { createErrorResponse, createSuccessResponse } = require('../../../response');
 
+async function updateEventDetails(crossEvent, event, updateData) {
+    // Attributes that are allowed to be updated in CrossPlatformEvent
+    const crossAttributes = ["hostName", "email", "address", "phoneNumber", "description", "eventTime"];
+
+    // Attributes that are allowed to be updated in Event
+    const eventAttributes = ["hostName", "email", "title", "price", "description", "createdOn", "date", "eventType", "startTime", "endTime", "eventTime", "address", "tags"];
+
+    // Proceed with the update logic, now both `crossAttributes` and `eventAttributes` are defined
+    // Update CrossPlatformEvent attributes if applicable
+    if (crossEvent) {
+        const crossUpdates = {};
+        Object.keys(updateData).forEach(key => {
+            if (crossAttributes.includes(key)) {
+                crossUpdates[key] = updateData[key];
+            }
+        });
+        await CrossPlatformEvent.findByIdAndUpdate(crossEvent._id, crossUpdates, { new: true });
+    }
+
+    // Update Event attributes if applicable
+    if (event) {
+        const eventUpdates = {};
+        Object.keys(updateData).forEach(key => {
+            if (eventAttributes.includes(key)) {
+                eventUpdates[key] = updateData[key];
+            }
+        });
+        await Event.findByIdAndUpdate(event._id, eventUpdates, { new: true });
+    }
+}
+
+
 module.exports = async (req, res) => {
     try {
+        const { eventId: hostId } = req.params;
 
-        const { eventId } = req.params;
-        console.log(eventId);
+        // Attempt to find events by hostId
+        const crossEvent = await CrossPlatformEvent.findOne({ hostId });
+        const event = await Event.findOne({ hostId });
 
-        let crossExists = true;
-        let spotExists = true;
-
-        // attempt to find event from id
-        const crossEvent = await CrossPlatformEvent.findById(eventId.hostId);
-        if (!crossEvent) {
-            crossExists = false;
-            console.log('Could not crossplatform event');
-            
+        if (!crossEvent && !event) {
+            throw new Error('Event not found in any database');
         }
 
-        console.log("CROSS EVENT SUCCESSFULLY FOUND");
+        // Update the details of both events if they exist
+        await updateEventDetails(crossEvent, event, req.body);
 
-        const event = await Event.findOne({ hostId: eventId.hostId });
-        if (!event) {
-          spotExists = false;
-          console.log('Could not find spotstitch event');
-        }
-
-        console.log("spot EVENT SUCCESSFULLY FOUND");
-
-        update(crossExists, spotExists, crossEvent, event);
-        
-    } catch (e) {
-        logger.error({ error: e.message }, "Could not find event")
-        res.status(400).json(createErrorResponse(400, "Could not find event"))
-
+        res.status(200).json(createSuccessResponse({ message: "Events updated successfully" }));
+    } catch (error) {
+        logger.error({ error: error.message }, "Could not update events");
+        res.status(400).json(createErrorResponse(400, error.message));
     }
-
-    const update = async (crossExists, spotExists, crossEvent, spotEvent) => {
-
-        const crossAttList = ["hostName", "email", "address", "phoneNumber", "description", "eventTime"];
-
-        const spotAttList = ["hostName", "email", "title", "price", "description", "createdOn", "date", "eventType", "startTime", "endTime", "eventTime", "address", "tags"];
-
-        const crossDataList = {
-            hostId: crossEvent.hostId,
-            hostName: crossEvent.hostName,
-            email: crossEvent.email,
-            address: crossEvent.address,
-            phoneNumber: crossEvent.phoneNumber,
-            description: crossEvent.description,
-            eventTime: crossEvent.eventTime
-        }
-
-        const spotDataList = {
-            hostId: spotEvent.hostId,
-            hostName: spotEvent.hostName,
-            email: spotEvent.email,
-            title: spotEvent.title,
-            price: spotEvent.price,
-            description: spotEvent.description,
-            createdOn: spotEvent.createdOn,
-            date: spotEvent.date,
-            eventType: spotEvent.eventType,
-            startTime: spotEvent.startTime,
-            endTime: spotEvent.endTime,
-            eventTime: spotEvent.eventTime,
-            address: spotEvent.address,
-            tags: spotEvent.tags
-        }
-
-    // Check and update CrossPlatformEvent attributes
-    if (crossExists) {
-        for (let attribute in req.body) {
-        if (crossAttList.includes(attribute)) {
-            crossDataList[attribute] = req.body[attribute];
-        }
-        }
-    
-        // Update CrossPlatformEvent in the database
-        try {
-        await CrossPlatformEvent.findOneAndUpdate({hostId: crossDataList.hostId}, crossDataList, {new: true});
-        } catch (error) {
-        console.error('Error updating CrossPlatformEvent:', error);
-        }
-    }
-    
-    // Check and update Event attributes
-    if (spotExists) {
-        for (let attribute in req.body) {
-        if (spotAttList.includes(attribute)) {
-            spotDataList[attribute] = req.body[attribute];
-        }
-        }
-    
-        // Update Event in the database
-        try {
-        await Event.findOneAndUpdate({hostId: spotDataList.hostId}, spotDataList, {new: true});
-        } catch (error) {
-        console.error('Error updating Event:', error);
-        }
-    }
-
-        }
-
-    
-
-
-}
+};
