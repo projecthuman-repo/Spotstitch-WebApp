ea5c1afef17dd22a1bcd3bf00ded2c000db68671
Jacky Shen <83739233+ElusoryPaths@users.noreply.github.com>
2024-04-09 17:01:26 -0400

    removed old socket code

--- Client/src/context/appContext.js
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-import React from "react";
-const SOCKET_URL = "http://localhost:5001";
-export const socket = io(SOCKET_URL, {autoConnect: false});
-// app context
-export const AppContext = React.createContext();
