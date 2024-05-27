671181094a2b19cdcf7341f8cf11eb10e0a4f9cf
LawrenceWan1996 <lawrence.dev@projecthumancity.com>
2024-05-27 10:54:14 -0400

    init

--- Client/src/pages/Start/AccountSetupA3.js
+++ Client/src/pages/Start/AccountSetupA3.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./AccountSetupA3.css";
 
-import sslogo from '../../assets/sslogo.png';
+import sslogo from "../../assets/sslogo.png";
 import holderimg from "../../assets/holderimg.png";
 
 import { Link, useNavigate } from "react-router-dom";
@@ -11,191 +11,207 @@ import { registerData, reset } from "../../features/User/registerSlice";
 import { useRegisterSpotstitchMutation } from "../../services/userApi";
 
 const AccountSetup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const registerForm = useSelector((state) => state.register);
+  const [registerUser, { isLoading: isUpdating, isError }] =
+    useRegisterUserMutation();
+  const [registerSpotstitch, {}] = useRegisterSpotstitchMutation();
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("")
-    const [username, setUsername] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [country, setCountry] = useState("");
-    const [state, setState] = useState("");
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const registerForm = useSelector(state => state.register)
-    const [registerUser, { isLoading: isUpdating, isError }] = useRegisterUserMutation()
-    const [registerSpotstitch, { }] = useRegisterSpotstitchMutation()
+  const selectCountries = ["Canada"];
+  const selectProvinces = [
+    "Alberta",
+    "British Columbia",
+    "Manitoba",
+    "New Brunswick",
+    "Newfoundland and Labrador",
+    "Northwest Territories",
+    "Nova Scotia",
+    "Nunavut",
+    "Ontario",
+    "Prince Edward Island",
+    "Quebec",
+    "Saskatchewan",
+    "Yukon",
+  ];
 
-    const selectCountries = ["Canada"]
-    const selectProvinces = [
-        "Alberta",
-        "British Columbia",
-        "Manitoba",
-        "New Brunswick",
-        "Newfoundland and Labrador",
-        "Northwest Territories",
-        "Nova Scotia",
-        "Nunavut",
-        "Ontario",
-        "Prince Edward Island",
-        "Quebec",
-        "Saskatchewan",
-        "Yukon"
-    ];
+  useEffect(() => {
+    setFirstName(localStorage.getItem("firstName"));
+    setLastName(localStorage.getItem("lastName"));
+    setUsername(localStorage.getItem("username"));
+    setPhoneNumber(localStorage.getItem("phoneNumber"));
+    setState(localStorage.getItem("province"));
+    setCountry(localStorage.getItem("country"));
+  }, []);
 
-    useEffect(() => {
-        setFirstName(localStorage.getItem("firstName"))
-        setLastName(localStorage.getItem("lastName"))
-        setUsername(localStorage.getItem("username"))
-        setPhoneNumber(localStorage.getItem("phoneNumber"))
-        setState(localStorage.getItem("province"))
-        setCountry(localStorage.getItem("country"))
-    }, [])
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // handle form submission here
+    await dispatch(
+      registerData({
+        username: event.target.username?.value,
+        firstName: event.target.firstName?.value,
+        lastName: event.target.lastName?.value,
+        phoneNumber: event.target.phoneNumber?.value,
+        country: event.target.country?.value,
+        province: event.target.province?.value,
+        appId: "spotstitch",
+      })
+    );
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        // handle form submission here
-        await dispatch(registerData({
-            username: event.target.username?.value,
-            firstName: event.target.firstName?.value,
-            lastName: event.target.lastName?.value,
-            phoneNumber: event.target.phoneNumber?.value,
-            country: event.target.country?.value,
-            province: event.target.province?.value,
-            appId: "spotstitch"
-        }))
+    try {
+      const registerServerResponse = await registerUser(registerForm);
+      console.log("success", registerServerResponse.data);
+      if (registerServerResponse.error) {
+        throw new Error(registerServerResponse.error.data.error);
+      }
+      const spotstitchServerResponse = await registerSpotstitch(registerForm);
+      console.log(spotstitchServerResponse.data);
+      dispatch(reset());
+      //localStorage.clear()
 
-        try {
-            const registerServerResponse = await registerUser(registerForm)
-            console.log("success", registerServerResponse.data)
-            if (registerServerResponse.error) {
-                throw new Error(registerServerResponse.error.data.error)
-            }
-            const spotstitchServerResponse = await registerSpotstitch(registerForm)
-            console.log(spotstitchServerResponse.data)
-            dispatch(reset())
-            //localStorage.clear()
-
-            navigate("/emailverification")
-        } catch (error) {
-            console.log("rejected", error.message)
-        }
+      navigate("/emailverification");
+    } catch (error) {
+      console.log("rejected", error.message);
     }
+  };
 
-    return (
-        <>
-            <div className="boss">
-                <div className="left">
-                    <img className="holderimg" src={holderimg} alt="holderimg" />
-                </div>
+  return (
+    <>
+      <div className="boss">
+        <div className="left">
+          <img className="holderimg" src={holderimg} alt="holderimg" />
+        </div>
 
+        <div className="right">
+          <div className="topspace"></div>
+          <img className="sslogo" src={sslogo} alt="sslogo" />
+          <div className="signuptitle">Account Information</div>
+          <div className="signupdescription">
+            We're designing and creating an open ecosystem; so innovative ideas
+            can turn into world changing action.
+          </div>
+          <form className="formsignin" onSubmit={handleSubmit}>
+            <input
+              className="email"
+              type="text"
+              placeholder="First Name"
+              id="firstName"
+              name="firstName"
+              required
+              value={firstName}
+              onChange={(event) => {
+                localStorage.setItem(event.target.name, event.target.value);
+                setFirstName(event.target.value);
+              }}
+            />
+            <br />
+            <input
+              className="email"
+              type="text"
+              placeholder="Last Name"
+              id="lastName"
+              name="lastName"
+              required
+              value={lastName}
+              onChange={(event) => {
+                localStorage.setItem(event.target.name, event.target.value);
+                setLastName(event.target.value);
+              }}
+            />
+            <br />
+            <input
+              className="email"
+              type="text"
+              placeholder="User Name"
+              id="username"
+              name="username"
+              required
+              value={username}
+              onChange={(event) => {
+                localStorage.setItem(event.target.name, event.target.value);
+                setUsername(event.target.value);
+              }}
+            />
+            <br />
+            <input
+              className="email"
+              type="tel"
+              placeholder="Phone Number"
+              id="phoneNumber"
+              name="phoneNumber"
+              pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+              required
+              value={phoneNumber}
+              onChange={(event) => {
+                localStorage.setItem(event.target.name, event.target.value);
+                setPhoneNumber(event.target.value);
+              }}
+            />
+            <br />
+            <select
+              className="email"
+              id="country"
+              name="country"
+              required
+              onChange={(event) => {
+                localStorage.setItem(event.target.name, event.target.value);
+                setCountry(event.target.value);
+              }}
+            >
+              <option value="" disabled selected>
+                Country
+              </option>
+              {selectCountries.map((country) => {
+                return <option value={country}>{country}</option>;
+              })}
+            </select>
+            <br />
+            <select
+              className="email"
+              id="province"
+              name="province"
+              required
+              onChange={(event) => {
+                localStorage.setItem(event.target.name, event.target.value);
+                setState(event.target.value);
+              }}
+            >
+              <option value="" disabled selected>
+                State/Province
+              </option>
+              {selectProvinces.map((province) => {
+                return <option value={province}>{province}</option>;
+              })}
+            </select>
+            <br />
+            <br />
+            <input
+              className="signup btn btn-primary"
+              type="submit"
+              value="Get Started"
+            ></input>
+          </form>
+          <br />
+          <br />
+          <br />
 
-                <div className="right">
-                    <div className="topspace"></div>
-                    <img className="sslogo" src={sslogo} alt="sslogo" />
-                    <div className="signuptitle">Account Information</div>
-                    <div className="signupdescription">We're designing and creating an open ecosystem; so innovative ideas can turn into world changing action.</div>
-                    <form className="formsignin" onSubmit={handleSubmit}>
+          <div className="policy2">
+            <span className="bold">Privacy Policy</span> and{" "}
+            <span className="bold">Terms of Service</span> apply.
+          </div>
 
-                        <input
-                            className="email"
-                            type="text"
-                            placeholder="First Name"
-                            id="firstName"
-                            name="firstName"
-                            required
-                            value={firstName}
-                            onChange={(event) => {
-                                localStorage.setItem(event.target.name, event.target.value)
-                                setFirstName(event.target.value)
-                            }}
-                        />
-                        <br />
-                        <input
-                            className="email"
-                            type="text"
-                            placeholder="Last Name"
-                            id="lastName"
-                            name="lastName"
-                            required
-                            value={lastName}
-                            onChange={(event) => {
-                                localStorage.setItem(event.target.name, event.target.value)
-                                setLastName(event.target.value)
-                            }}
-                        />
-                        <br />
-                        <input
-                            className="email"
-                            type="text"
-                            placeholder="User Name"
-                            id="username"
-                            name="username"
-                            required
-                            value={username}
-                            onChange={(event) => {
-                                localStorage.setItem(event.target.name, event.target.value)
-                                setUsername(event.target.value)
-                            }}
-                        />
-                        <br />
-                        <input
-                            className="email"
-                            type="tel"
-                            placeholder="Phone Number"
-                            id="phoneNumber"
-                            name="phoneNumber"
-                            pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
-                            required
-                            value={phoneNumber}
-                            onChange={(event) => {
-                                localStorage.setItem(event.target.name, event.target.value)
-                                setPhoneNumber(event.target.value)
-                            }}
-                        />
-                        <br />
-                        <select className="email"
-                            id="country"
-                            name="country"
-                            required
-                            onChange={(event) => {
-                                localStorage.setItem(event.target.name, event.target.value)
-                                setCountry(event.target.value)
-                            }}
-                        >
-                            <option value="" disabled selected>Country</option>
-                            {selectCountries.map((country) => {
-                                return <option value={country}>{country}</option>
-                            })}
-                        </select>
-                        <br />
-                        <select className="email"
-                            id="province"
-                            name="province"
-                            required
-                            onChange={(event) => {
-                                localStorage.setItem(event.target.name, event.target.value)
-                                setState(event.target.value)
-                            }}>
-                            <option value="" disabled selected>State/Province</option>
-                            {selectProvinces.map((province) => {
-                                return <option value={province}>{province}</option>
-                            })}
-                        </select>
-                        <br /><br />
-                        <input className="signup" type="submit" value="Get Started"></input>
-                    </form>
-                    <br /><br /><br />
+          <div className="botspace"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-                    <div className="policy2"><span className="bold">Privacy Policy</span> and <span className="bold">Terms of Service</span> apply.</div>
-
-
-                    <div className="botspace"></div>
-
-                </div>
-
-
-            </div>
-        </>
-    )
-}
-
-export default AccountSetup
+export default AccountSetup;
--- Client/src/pages/Start/Start.js
+++ Client/src/pages/Start/Start.js
@@ -2,11 +2,11 @@ import React from "react";
 import { useState } from "react";
 import "./Start.css";
 import holderimg from "../../assets/holderimg.png";
-import sslogo from '../../assets/sslogo.png';
-import orline from '../../assets/orline.png';
-import googlesignin from '../../assets/googlesignin.png';
-import facesignin from '../../assets/facesignin.png';
-import projectsignin from '../../assets/projectsignin.png';
+import sslogo from "../../assets/sslogo.png";
+import orline from "../../assets/orline.png";
+import googlesignin from "../../assets/googlesignin.png";
+import facesignin from "../../assets/facesignin.png";
+import projectsignin from "../../assets/projectsignin.png";
 import { Link, useNavigate } from "react-router-dom";
 import { useLoginUserMutation } from "../../services/loginApi";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,111 +14,110 @@ import { login, setUserData } from "../../features/User/userSlice";
 import { useGetUserProfileMutation } from "../../services/userApi";
 
 const Start = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginUser, { isLoading: isUpdating, isError }] =
+    useLoginUserMutation();
+  const [getUserProfile, {}] = useGetUserProfileMutation();
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loginUser, { isLoading: isUpdating, isError }] = useLoginUserMutation();
-    const [getUserProfile, { }] = useGetUserProfileMutation();
-    const user = useSelector(state => state.user)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!password && !email) return;
+    // handle form submission here
+    try {
+      const secret = {
+        username: email,
+        password: password,
+      };
+      const res = await loginUser(secret);
+      if (res.error) throw new Error(res.error.data.error);
 
+      const token = res.data?.token;
+      if (!token) throw new Error("Token not found");
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        if (!password && !email) return
-        // handle form submission here
-        try {
-            const secret = {
-                username: email,
-                password: password
-            }
-            const res = await loginUser(secret)
-            if (res.error) throw new Error(res.error.data.error)
-            
-            const token = res.data?.token
-            if (!token) throw new Error("Token not found")
+      // save login token for future access
+      await dispatch(login({ token: token }));
 
-            // save login token for future access
-            await dispatch(login({ token: token }))
+      // get data from spotstitch database
+      const userData = await getUserProfile();
+      console.log(userData);
+      if (userData.data?.status != "error")
+        await dispatch(setUserData(userData.data?.user));
+      else throw new Error(data.error);
 
-            // get data from spotstitch database
-            const userData = await getUserProfile()
-            console.log(userData)
-            if (userData.data?.status != "error") await dispatch(setUserData(userData.data?.user))
-            else throw new Error(data.error)
-
-            // navigate to home route
-            navigate("/")
-        } catch (error) {
-            console.log("rejected", error.message)
-        } 
+      // navigate to home route
+      navigate("/");
+    } catch (error) {
+      console.log("rejected", error.message);
     }
+  };
 
-    return (
+  return (
+    <div className="boss">
+      <div className="left">
+        <img className="holderimg" src={holderimg} alt="holderimg" />
+      </div>
+      <div className="right">
+        <div className="topspace"></div>
+        <img className="sslogo" src={sslogo} alt="sslogo" />
+        <div className="titlen">SPOTSTITCH</div>
+        <div />
 
-        <div className="boss">
-            <div className="left">
-                <img className="holderimg" src={holderimg} alt="holderimg" />
-            </div>
-            <div className="right">
-                <div className="topspace"></div>
-                <img className="sslogo" src={sslogo} alt="sslogo" />
-                <div className="titlen">SPOTSTITCH</div>
-                <div />
+        <form className="formsignin" onSubmit={handleSubmit}>
+          <input
+            className="email"
+            type="text"
+            placeholder="Email or Username"
+            id="email"
+            name="email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
 
-                <form className="formsignin" onSubmit={handleSubmit}>
+          <input
+            className="password"
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
 
-                    <input 
-                    className="email" 
-                    type="text" 
-                    placeholder="     Email or Username" 
-                    id="email" 
-                    name="email" 
-                    onChange={(event) => setEmail(event.target.value)} />
+          <input
+            className="signin btn btn-primary"
+            type="submit"
+            value="Sign In"
+          />
+        </form>
 
-                    <input 
-                    className="password" 
-                    type="password" 
-                    placeholder="     Password" 
-                    id="password" 
-                    name="password" 
-                    onChange={(event) => setPassword(event.target.value)}/>
+        <img className="lineor" src={orline} alt="orline" />
 
-                    <input className="signin" type="submit" value="Sign In" />
-                </form>
+        <button className="googlebutton">
+          <img src={googlesignin} alt="googlesignin" border="0" />
+        </button>
 
-                <img className="lineor" src={orline} alt="orline" />
+        <button className="facebookbutton">
+          <img src={facesignin} alt="facebooksignin" border="0" />
+        </button>
 
-                <button className="googlebutton">
-                    <img src={googlesignin} alt="googlesignin" border="0" />
-                </button>
+        <button className="projectbutton">
+          <img src={projectsignin} alt="projectsignin" border="0" />
+        </button>
 
-                <button className="facebookbutton">
-                    <img src={facesignin} alt="facebooksignin" border="0" />
-                </button>
-
-                <button className="projectbutton">
-                    <img src={projectsignin} alt="projectsignin" border="0" />
-                </button>
-
-
-
-
-
-                <div className="needaccountbox">
-                    Need an account?
-                </div>
-                <Link to='/usersignup' className="createaccount">Create your account</Link>
-
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-
-
+        <div className="needaccountbox">
+          Need an account?{" "}
+          <Link to="/usersignup" className="createaccount">
+            Create your account
+          </Link>
         </div>
-    )
-}
 
-export default Start
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
