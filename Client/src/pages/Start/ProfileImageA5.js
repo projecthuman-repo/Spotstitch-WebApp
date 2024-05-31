37b2c87344657418003ff2ac0e763042bd696e45
LawrenceWan1996 <lawrence.dev@projecthumancity.com>
2024-05-31 07:02:10 -0400

    Register page mostly done, without password and email validation

--- Client/src/pages/Start/ProfileImageA5.js
+++ Client/src/pages/Start/ProfileImageA5.js
@@ -1,104 +1,129 @@
 import React, { useRef, useState } from "react";
 import "./ProfileImageA5.css";
 import holderimg from "../../assets/holderimg.png";
-import sslogo from '../../assets/sslogo.png';
-import photo from '../../assets/photo.png';
+import sslogo from "../../assets/sslogo.png";
+import photo from "../../assets/photo.png";
 
 import { Form, Link, useNavigate } from "react-router-dom";
 import { useUpdatePictureMutation } from "../../services/userApi";
 
 const ProfileImage = () => {
-    const [image, setImage] = useState()
-    const imageRef = useRef()
-    const [updatePicture, { }] = useUpdatePictureMutation()
-    const navigate = useNavigate()
+  const [image, setImage] = useState();
+  const imageRef = useRef();
+  const [updatePicture, {}] = useUpdatePictureMutation();
+  const navigate = useNavigate();
 
-    const convertToBase64 = async () => {
-        const reader = new FileReader()
-        reader.readAsDataURL(imageRef.current.files[0])
-        reader.onload = () => {
-            setImage(reader.result)
-        }
+  const convertToBase64 = async () => {
+    const reader = new FileReader();
+    reader.readAsDataURL(imageRef.current.files[0]);
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+  };
+
+  function handleChangeImage() {
+    imageRef.current.click();
+  }
+
+  async function nextPage() {
+    navigate("/accounttype");
+  }
+
+  async function handleSubmit() {
+    try {
+      const res = await updatePicture({ picture: image });
+      if (res.error) throw new Error(res.error.data.error.message);
+      if (res.data?.status == "ok") {
+        await dispatch(setUserData({ picture: image }));
+      }
+      nextPage();
+    } catch (error) {
+      console.log("rejected", error.message);
     }
+  }
 
-    function handleChangeImage() {
-        imageRef.current.click()
-    }
+  return (
+    <>
+      <div className="boss">
+        <div className="left">
+          <img className="holderimg" src={holderimg} alt="holderimg" />
+        </div>
 
-    async function nextPage() {
-        navigate('/accounttype')
-    }
+        <div className="right">
+          <div className="topspace"></div>
+          <br />
+          <br />
 
-    async function handleSubmit() {
-        try {
-            const res = await updatePicture({ picture: image })
-            if (res.error) throw new Error(res.error.data.error.message)
-            if (res.data?.status == "ok") {
-                await dispatch(setUserData({ picture: image }))
-            }
-            nextPage()
-        } catch (error) {
-            console.log('rejected', error.message)
-        }
+          <img className="sslogo" src={sslogo} alt="sslogo" />
+          <div className="signuptitle">Add a profile photo.</div>
+          <br />
+          <div className="signupdescription">
+            We're designing and creating an open ecosystem; so innovative ideas
+            can turn into world changing action.{" "}
+          </div>
 
-    }
+          <br />
 
-    return (
-        <>
-            <div className="boss">
-                <div className="left">
-                    <img className="holderimg" src={holderimg} alt="holderimg" />
-                </div>
+          <input
+            hidden
+            name="picture"
+            type="file"
+            ref={imageRef}
+            accept=".png,.jpg,.jpeg,.webp"
+            onChange={(e) => {
+              console.log(e.target.files[0]);
+              if (e.target.files[0]) {
+                let buffer = e.target.files[0];
+                convertToBase64();
+              }
+            }}
+          />
 
+          <img
+            src={image || photo}
+            alt={"photo"}
+            className="photo avatar"
+            onClick={() => {
+              handleChangeImage();
+            }}
+          />
+          <br />
+          <br />
+          <br />
+          <div className="skipnextbox">
+            <button
+              className="skipbutton"
+              onClick={() => {
+                nextPage();
+              }}
+            >
+              Skip
+            </button>
 
-                <div className="right">
-                    <div className="topspace"></div><br /><br />
+            <button
+              className="nextbutton btn btn-primary"
+              onClick={() => {
+                handleSubmit();
+              }}
+            >
+              Next
+            </button>
+          </div>
 
-                    <img className="sslogo" src={sslogo} alt="sslogo" />
-                    <div className="signuptitle">Add a profile photo.</div>
-                    <br />
-                    <div className="signupdescription">We're designing and creating an open ecosystem; so innovative ideas can turn into world changing action. </div>
+          <br />
+          <br />
+          <br />
 
-                    <br />
+          <div className="policy2">
+            <span className="bold">Privacy Policy</span> and{" "}
+            <span className="bold">Terms of Service</span> apply.
+          </div>
 
-                    <input
-                        hidden
-                        name="picture"
-                        type='file'
-                        ref={imageRef}
-                        accept=".png,.jpg,.jpeg,.webp"
-                        onChange={(e) => {
-                            console.log(e.target.files[0])
-                            if (e.target.files[0]) {
-                                let buffer = e.target.files[0]
-                                convertToBase64()
-                            }
+          <div className="botspace"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-                        }} />
-
-                    <img src={image || photo} alt={'photo'} className="photo avatar" onClick={() => { handleChangeImage() }} />
-                    <br /><br /><br />
-                    <div className="skipnextbox">
-                        <button className="skipbutton" onClick={() => { nextPage() }}>Skip</button>
-
-                        <button className="nextbutton" onClick={() => { handleSubmit() }}>Next</button>
-
-                    </div>
-
-                    <br /><br /><br />
-
-                    <div className="policy2">
-                        <span className="bold">Privacy Policy</span> and <span className="bold">Terms of Service</span> apply.
-                    </div>
-
-                    <div className="botspace"></div>
-
-                </div>
-
-
-            </div>
-        </>
-    )
-}
-
-export default ProfileImage
+export default ProfileImage;
--- Client/src/pages/Start/UserSignUpA2.js
+++ Client/src/pages/Start/UserSignUpA2.js
@@ -1,148 +1,209 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./UserSignUpA2.css";
-import HumanCityLogo from '../../assets/HumanCityLogo.png';
-import sslogo from '../../assets/sslogo.png';
-import orline from '../../assets/orline.png';
+import HumanCityLogo from "../../assets/HumanCityLogo.png";
+import sslogo from "../../assets/sslogo.png";
+import orline from "../../assets/orline.png";
 
 import holderimg from "../../assets/holderimg.png";
-import googlesignin from '../../assets/googlesignin.png';
-import facesignin from '../../assets/facesignin.png';
-import projectsignin from '../../assets/projectsignin.png';
+import googlesignin from "../../assets/googlesignin.png";
+import facesignin from "../../assets/facesignin.png";
+import projectsignin from "../../assets/projectsignin.png";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { registerData } from "../../features/User/registerSlice";
+import { Overlay, Tooltip } from 'react-bootstrap';
+
+
 
 const UserSignUp = () => {
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [matching, setMatching] = useState(true)
-    const [valid, setValid] = useState(true)
-    const [showPassword, setShowPassword] = useState(false)
-    const [acceptedTerms, setAcceptedTerms] = useState(false);
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const registerForm = useSelector(state => state.register)
-    /* based on login servers, Passwords must be between 8-10 characters long, 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [matching, setMatching] = useState(true);
+  const [valid, setValid] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const [acceptedTerms, setAcceptedTerms] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const registerForm = useSelector((state) => state.register);
+  /* based on login servers, Passwords must be between 8-10 characters long, 
         have at least one uppercase letter, lowercase letter, number and symbol */
-    const check = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/
+  const check =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/;
 
-    useEffect(() => {
-        setEmail(localStorage.getItem("email"))
-    }, [])
+  useEffect(() => {
+    setEmail(localStorage.getItem("email"));
+  }, []);
 
-    useEffect(() => {
-        if (check.test(password)) { setValid(true) }
-        else { setValid(false) }
-
-        if (password == confirmPassword) setMatching(true)
-        else setMatching(false)
-    }, [password, confirmPassword])
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // handle form submission here
-        if (!matching || !acceptedTerms || !valid) return
-        dispatch(registerData({
-            email: event.target.email.value,
-            password: event.target.password.value
-        }))
-        navigate("/accountsetup")
+  useEffect(() => {
+    if (check.test(password)) {
+      setValid(true);
+    } else {
+      setValid(false);
     }
 
-    return (
-        <>
-            <div className="boss">
-                <div className="left">
-                    <img className="holderimg" src={holderimg} alt="holderimg" />
-                </div>
+    if (password == confirmPassword) setMatching(true);
+    else setMatching(false);
+  }, [password, confirmPassword]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle form submission here
+    if (!matching || !acceptedTerms || !valid) return;
+    dispatch(
+      registerData({
+        email: event.target.email.value,
+        password: event.target.password.value,
+      })
+    );
+    navigate("/accountsetup");
+  };
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+  const target = useRef(null);
+  return (
+    <>
+      <div className="boss">
+        <div className="left">
+          <img className="holderimg" src={holderimg} alt="holderimg" />
+        </div>
+
+        <div className="right">
+          <div className="topspace"></div>
+          <img className="sslogo" src={sslogo} alt="sslogo" />
+          <div className="signuptitle">Let's get you stitched in!</div>
+          <div className="signupdescription">
+            We're designing and creating an open ecosystem; so innovative ideas
+            can turn into world changing action.{" "}
+          </div>
+
+          <form className="formsignin" onSubmit={handleSubmit}>
+            <input
+              className="email"
+              type="text"
+              placeholder="Email"
+              id="email"
+              name="email"
+              required
+              value={email}
+              onChange={(event) => {
+                // localStorage.setItem("email", event.target.value);
+                // setEmail(event.target.value);
+                if (check.test(event.target.value)) {
+                  localStorage.setItem("email", event.target.value);
+                  setEmail(event.target.value);
+                } else {
+                  console.log('Invalid email format');
+                }
+              }}
+            />
+            {/* <br /> */}
+            {password && confirmPassword && !matching && (
+              <>
+
+              <p className="passwordMatch">
+                passwords do not match
+              </p>
+              </>
+            )}
+            
+            <input
+            ref={target}
+            className="password"
+            type={showPassword ? "text" : "password"}
+            onFocus={() => setIsPasswordFocused(true)}
+            onBlur={() => setIsPasswordFocused(false)}
+            placeholder="Password"
+            id="password"
+            name="password"
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+            <Overlay target={target.current} show={isPasswordFocused} placement="left" >
+              {(props) => (
+                <Tooltip id="button-tooltip" {...props} className="password-tooltip">
+                  <ul className="passwordRequirement">
+                    <li>Passwords must be between 8-10 characters long</li>
+                    <li>One uppercase letter</li>
+                    <li>One lowercase letter</li>
+                    <li>One number</li>
+                    <li>One symbol</li>
+                  </ul>
+                </Tooltip>
+              )}
+            </Overlay>
+
+            {/* <br/> */}
+            <input
+              className="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Confirm Password"
+              id="confirmpassword"
+              name="confirmpassword"
+              value={confirmPassword}
+              onChange={(event) => setConfirmPassword(event.target.value)}
+            />
+            <br />
+            {/* {!valid && isPasswordFocused && (
+              <>
+              <ul className="passwordRequirement">
+                <li>Passwords must be between 8-10 characters long</li>
+                <li>One uppercase letter</li>
+                <li>One lowercase letter</li>
+                <li>One number</li>
+                <li>One symbol</li>
+              </ul>
+                <br />
+              </>
+            )} */}
 
 
-                <div className="right">
-                    <div className="topspace"></div>
-                    <img className="sslogo" src={sslogo} alt="sslogo" />
-                    <div className="signuptitle">Let's get you stitched in!</div>
-                    <div className="signupdescription">We're designing and creating an open ecosystem; so innovative ideas can turn into world changing action. </div>
-
-                    <form className="formsignin" onSubmit={handleSubmit}>
-                        <input
-                            className="email"
-                            type="text"
-                            placeholder="     Email"
-                            id="email"
-                            name="email"
-                            required
-                            value={email}
-                            onChange={(event) => {
-                                localStorage.setItem("email", event.target.value)
-                                setEmail(event.target.value)
-                            }}
-                        />
-                        <br />
-                        <input
-                            className="password"
-                            type={showPassword ? "text" : "password"}
-                            placeholder="     Password"
-                            id="password"
-                            name="password"
-                            required
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)}
-                        />
-                        {!valid && <p className="">Passwords must be between 8-10 characters long,
-                            have at least one uppercase letter, lowercase letter, number and symbol</p>}
-                        <br />
-                        <input
-                            className="password"
-                            type={showPassword ? "text" : "password"}
-                            placeholder="     Confirm Password"
-                            id="confirmpassword"
-                            name="confirmpassword"
-                            value={confirmPassword}
-                            onChange={(event) => setConfirmPassword(event.target.value)}
-                        />
-                        {!matching && <p className="">passwords do not match</p>}
-                        <br />
-                        <div className="checkarea">
-                            <input
-                                className="checkboxpolicy"
-                                type="checkbox"
-                                checked={acceptedTerms}
-                                onChange={(event) => setAcceptedTerms(event.target.checked)}
-                            />
-                            <div className="policy">I accept Spotstitch's <span className="bold">Privacy Policy</span> and <span className="bold">Terms of Service</span>.</div>
-                        </div>
-                        <br />
-                        <input className="signup" type="submit" value="Join Spotstitch"></input>
-
-                    </form>
-                    <br />
-                    <img className="lineor" src={orline} alt="orline" />
-                    <br />
-                    <button className="googlebutton">
-                        <img src={googlesignin} alt="googlesignin" border="0" />
-                    </button>
-                    <br />
-                    <button className="facebookbutton">
-                        <img src={facesignin} alt="facebooksignin" border="0" />
-                    </button>
-                    <br />
-                    <button className="projectbutton">
-                        <img src={projectsignin} alt="projectsignin" border="0" />
-                    </button>
-                    <br />
-                    <div className="policy2"><span className="bold">Privacy Policy</span> and <span className="bold">Terms of Service</span> apply.</div>
-
-
-                    <div className="botspace"></div>
-
-                </div>
-
-
+            <br />
+            <div className="checkarea">
+              <input
+                className="checkboxpolicy"
+                type="checkbox"
+                checked={acceptedTerms}
+                onChange={(event) => setAcceptedTerms(event.target.checked)}
+              />
+              <div className="policy">
+                I accept Spotstitch's{" "}
+                <span className="bold">Privacy Policy</span> and{" "}
+                <span className="bold">Terms of Service</span>.
+              </div>
             </div>
-        </>
-    )
-}
+            <br />
+            <input
+              className="signup btn btn-primary"
+              type="submit"
+              value="Join Spotstitch"
+            ></input>
+          </form>
+          <br />
+          <img className="lineor" src={orline} alt="orline" />
+          <br />
+          <button className="googlebutton">
+            <img src={googlesignin} alt="googlesignin" border="0" />
+          </button>
+          <br />
+          <button className="facebookbutton">
+            <img src={facesignin} alt="facebooksignin" border="0" />
+          </button>
+          <br />
+          <button className="projectbutton">
+            <img src={projectsignin} alt="projectsignin" border="0" />
+          </button>
+          <br />
+          <div className="policy2">
+            <span className="bold">Privacy Policy</span> and{" "}
+            <span className="bold">Terms of Service</span> apply.
+          </div>
 
-export default UserSignUp
+          <div className="botspace"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserSignUp;
