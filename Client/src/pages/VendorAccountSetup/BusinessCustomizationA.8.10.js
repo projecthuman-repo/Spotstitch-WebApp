6ffdeb0c4b84a9e11c576b03d6c6afd4c8606a7d
LawrenceWan1996 <lawrence.dev@projecthumancity.com>
2024-06-13 09:41:33 -0400

    until 8.10

--- Client/src/pages/VendorAccountSetup/BusinessCustomizationA.8.10.js
+++ Client/src/pages/VendorAccountSetup/BusinessCustomizationA.8.10.js
@@ -1,7 +1,57 @@
-function BusinessCustomization(){
-    return (
+import { useRef, useState } from "react";
+import photo from "../../assets/User Picture.png";
+import pencil from "../../assets/Pencil Icon.png";
+
+function BusinessCustomization() {
+  const [image, setImage] = useState();
+  const imageRef = useRef();
+
+  const handleChangeImage = async () => {
+    const file = imageRef.current.files[0];
+    if (file) {
+      const base64Image = await convertToBase64(file);
+      setImage(base64Image);
+    }
+  };
+
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  };
+
+  return (
     <>
-    </>)
+      <h1>Business Customization</h1>
+      <p>Show off your business with some pictures!</p>
+      <div>
+        <input
+          type="file"
+          ref={imageRef}
+          style={{ display: "none" }}
+          onChange={handleChangeImage}
+        />
+        <img
+          src={pencil}
+          alt={"photo"}
+          className="photo avatar"
+          onClick={() => imageRef.current.click()}
+          style={{ backgroundColor: "transparent" }}
+        />
+        <div></div>
+        <img
+          src={image || photo}
+          alt={"photo"}
+          className="photo avatar"
+          onClick={() => imageRef.current.click()}
+          style={{ backgroundColor: "transparent" }}
+        />
+      </div>
+    </>
+  );
 }
 
-export default BusinessCustomization
+export default BusinessCustomization;
