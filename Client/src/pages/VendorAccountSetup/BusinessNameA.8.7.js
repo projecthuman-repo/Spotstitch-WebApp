114488b963d5b09509243ebfeec7e224f22a9f11
LawrenceWan1996 <lawrence.dev@projecthumancity.com>
2024-06-14 17:20:44 -0400

    name business partially done

--- Client/src/pages/VendorAccountSetup/BusinessNameA.8.7.js
+++ Client/src/pages/VendorAccountSetup/BusinessNameA.8.7.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ProgressBar from "./progressBar";
+import "./BusinessNameA.8.7.css";
 function BusinessName() {
   //back button
   //save and continue
@@ -9,24 +10,28 @@ function BusinessName() {
   return (
     <>
       <ProgressBar />
-      <div className="space1"></div>
-      <h1 className="progress-page-title">Name Your Business</h1>
-      <p className="small-description">
-        No rush! You may use draft names for now and changes it later.
-      </p>
-      <div className="form-container">
-        <input
-          placeholder="Enter your business name"
-          onChange={handleSetBusinessName}
-        ></input>
-        <ul>
-          <li>Between 4-20 characters</li>
-          <li>No special characters, or accented letters</li>
-        </ul>
-      </div>
+      <div className="main-form">
+        <div className="heading">
+          <h1 className="heading-busiessname">Name Your Business</h1>
+          <p className="small-description-busiessname">
+            No rush! You may use draft names for now and changes it later.
+          </p>
+        </div>
+        <div className="form-container">
+          <input
+            className="Text-input "
+            placeholder="Enter your business name"
+            onChange={handleSetBusinessName}
+          ></input>
+          <ul className="input-requirement">
+            <li>Between 4-20 characters</li>
+            <li>No special characters, or accented letters</li>
+          </ul>
+        </div>
 
-      <button className="back-button">Back</button>
-      <button className="save&continue">Save&Continue</button>
+        {/* <button className="back-button">Back</button> */}
+        {/* <button className="save&continue">Save&Continue</button> */}
+      </div>
     </>
   );
 }
